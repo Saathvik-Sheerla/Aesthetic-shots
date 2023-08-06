0d2d0a03dee133f3a538eb61b12a25fde1684b36
Saathvik-Sheerla <140387813+Saathvik-Sheerla@users.noreply.github.com>
2023-08-06 10:10:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 from PIL import Image 
 
+#-----------------------menu style start----------------------------------------
+
+st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+
+st.markdown(st_style , unsafe_allow_html=True)
+
+#----------------------menu style end-------------------------------------------
+
 
 st.title("AESTHETIC SHOTS")
 
 cc1, cc2 ,cc3 = st.columns(3)
 
 def bttn():
+	
 	ans_1 = st.button("want to see more")
 	if ans_1 == True:
 		return True
@@ -18,21 +33,30 @@ st.write("_Passionate about automobile photography_")
 
 c1, c2 ,c3 = st.columns(3)
 
+
 g11 = Image.open('python/Showcase/Aesthetic/325575177_1216507902615616_5191955527552643508_n_564611968922794.jpg')
 g21 = Image.open('python/Showcase/Aesthetic/IMG_0003.jpg')
 g31 = Image.open('python/Showcase/Aesthetic/SUN09745.jpg')
 g41 = Image.open('python/Showcase/Aesthetic/IMG_1769.jpg')
+g51 = Image.open('python/Showcase/Aesthetic/PicsArt_21-10-12_11-25-59-780.jpg')
+g61 = Image.open('python/Showcase/Aesthetic/IMG_6146-26_1.jpg')
 
 g12 = Image.open('python/Showcase/Aesthetic/32323 (7 of 9).jpg')
 g22 = Image.open('python/Showcase/Aesthetic/IMG_1728.jpg')
 g32 = Image.open('python/Showcase/Aesthetic/IMG_6191-43.jpg')
 g42 = Image.open('python/Showcase/Aesthetic/IMG_0478.JPG')
 g52 = Image.open('python/Showcase/Aesthetic/IMG_1498.jpg')
+g62 = Image.open('python/Showcase/Aesthetic/IMG_9870.jpg')
+g72 = Image.open('python/Showcase/Aesthetic/IMG_9916.jpg')
+g82 = Image.open('python/Showcase/Aesthetic/IMG_1713.jpg')
 
 g13 = Image.open('python/Showcase/Aesthetic/IMG_20230124_204445-2.jpg')
 g23 = Image.open('python/Showcase/Aesthetic/32323 (25 of 30).jpg')
 g33 = Image.open('python/Showcase/Aesthetic/IMG_1652.jpg')
 g43 = Image.open('python/Showcase/Aesthetic/IMG_6153-29.jpg')
+g53 = Image.open('python/Showcase/Aesthetic/32323 (10 of 30).jpg')
+g63 = Image.open('python/Showcase/Aesthetic/IMG_1701.jpg')
+g73 = Image.open('python/Showcase/Aesthetic/IMG_6182-41.jpg')
 
 
 with c1:
@@ -60,12 +84,22 @@ with c3:
 if bttn() == True:
 	with c1:
 		st.image(g41)
+		st.image(g51)
+		st.image(g61)
+
 	with c2:
 		st.image(g52)
+		st.image(g62)
+		st.image(g72)
+		st.image(g82)
+
 	with c3:
 		st.image(g43)
+		st.image(g53)
+		st.image(g63)
+		st.image(g73)
 
-
+#-----------------------------------------contact form start-----------------------------------------------------
 
 contact_form = """
 <form action="https://formsubmit.co/shravyarinky29@gmail.com" method="POST">
@@ -85,19 +119,5 @@ def local_css(file_name):
 
 local_css("style/style.css.txt")
 
-st_style = """
-                <style>
-                #MainMenu{visibility: hidden;}
-                footer {visibility: hidden;}
-				footer:after{
-					content:'by saathvik';
-					display:block;
-					position:relative;
-					color:tamoto;
-					padding:5px;
-					top:3px;}
-                header {visibility: hidden}
-                </style>
-                """
+#-----------------------------------------contact form end-------------------------------------------------------------
 
-st.markdown(st_style , unsafe_allow_html=True)
